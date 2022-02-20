6ff6b14c07ac6cadb36920e54a3e66248ecbca5b
johbess <johan.besseling@gmail.com>
2022-02-20 13:57:53 +0100

    initial commit of spring boot basic project

---
+++ src/main/java/com/MusicMash/MusicMashApplication.java
@@ -0,0 +1,13 @@
+package com.MusicMash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMashApplication.class);
+	}
+
+}
---
+++ src/test/java/com/MusicMash/MusicMashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MusicMash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
